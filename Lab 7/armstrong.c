401f469808ce213277e70a3475f7d1a29195db66
Vatsal Parmar <vatsal6940@gmail.com>
2024-08-17 10:25:01 +0530

    Upload Lab 7

---
+++ Lab 7/armstrong.c
@@ -0,0 +1,41 @@
+#include<stdio.h> 
+#include<math.h> 
+int main(){
+
+    int n,rem,sum=0,temp,count=0;    
+    printf("enter the number:");    
+    scanf("%d",&n);    
+
+    temp=n;
+   
+while(n>0){
+        rem=n%10;
+        if(n!=0){
+            count++;
+        }
+        n=n/10;
+    }
+
+    printf("%d",count);
+    
+    while(n>0){
+        int rem1=n%10;
+        sum=sum+(pow(rem1,count));
+        n=n/10;
+    }
+    
+    /*while(n>0){    
+        s=n%10;    
+        sum=sum+(pow(s,count));    
+        n=n/10;    
+    }*/
+        if(temp==sum){
+            printf("Armstrong number.");    
+        }
+        else{    
+            printf("Not Armstrong number.");
+                
+}   
+return 0;
+
+}
